cdcec865bf1a311d08c581e997b7e6e465055302
global <theacheng.g6@gmail.com>
2022-03-14 16:49:09 +0700

    add comment
    https://guides.rubyonrails.org/getting_started.html#adding-a-second-model

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,8 @@ Rails.application.routes.draw do
   resources :articles
   get "/articles", to: "articles#index"
   get "/articles/:id", to: "articles#show"
+
+  resources :articles do
+    resources :comments
+  end
 end
