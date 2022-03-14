9cc84a47520d54572559fb0e6bbbea98092bfa4d
global <theacheng.g6@gmail.com>
2022-03-14 17:39:59 +0700

    add authentication on transaction except viewing
    https://guides.rubyonrails.org/getting_started.html#security

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,6 @@
 class ArticlesController < ApplicationController
+  http_basic_authenticate_with name: "admin", password: "123456", except: [:index, :show]
+
   def index
     @articles = Article.all
   end
